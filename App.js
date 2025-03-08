7719c5db258043d355baf2a242b4d3b03e703612
karandesai2005 <karanishudesai2@gmail.com>
2025-03-08 11:40:49 +0530

    s

--- App.js
+++ App.js
@@ -1,8 +1,9 @@
-import React, { useEffect } from 'react';
+import 'react-native-gesture-handler';
+import React from 'react';
 import { NavigationContainer } from '@react-navigation/native';
 import AppNavigator from './src/navigation/AppNavigator';
+
 export default function App() {
-  
   return (
     <NavigationContainer>
       <AppNavigator />
